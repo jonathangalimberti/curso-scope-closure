d9f840a2c90a6f344e893d2478b1c1a0dc0c7813
jona-11 <jonathangalimberti@hotmail.com>
2022-08-23 20:07:18 -0300

    ejemplificacion de hoisting

---
+++ src/hoisting/hoisting.js
@@ -0,0 +1,15 @@
+/* var nameOfDog;
+
+console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+console.log(nameOfDog); */
+
+nameOfDog();
+
+function nameOfDog(){
+
+    console.log
+    ( `el mejor perrito del mundo es ${elmo}`);
+}
+
+var elmo= 'elmito';
